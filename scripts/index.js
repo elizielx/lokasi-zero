f6062f30ff3c59eb3875d3c0fdce88b64ec2d172
elizielx <yehezkieldio35@gmail.com>
2023-12-01 13:24:59 +0800

    feat: some

--- scripts/index.js
+++ scripts/index.js
@@ -5,11 +5,25 @@ document.addEventListener("DOMContentLoaded", function () {
     const toggleButton = document.getElementById("toggleButton");
     const isDarkMode = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
 
+    const navigation = document.querySelector(".navigation");
+    const sticky = navigation.offsetTop;
+
+    function handleScroll() {
+        if (window.scrollY > sticky) {
+            navigation.classList.add("sticky");
+        } else {
+            navigation.classList.remove("sticky");
+        }
+    }
+
     if (isDarkMode) {
         wrapper.classList.add("dark-mode");
     } else {
         wrapper.classList.add("light-mode");
     }
+
+    // listen for scroll down and make nav sticky
+    window.addEventListener("scroll", handleScroll);
 
     // toggle dark mode / light mode
     toggleButton.addEventListener("click", function () {
